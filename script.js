0c36891786cbfdc891b15442121379b855e45c27
VladOneX <vladthefirstone@gmail.com>
2021-06-10 13:01:17 +0300

    v1.2

--- script.js
+++ script.js
@@ -6,36 +6,36 @@ $(document).ready(function() {
 
         /* Username validation */
         if (username == null || username == '') {
-            $('input[type="text"]').css("border","1px solid red");
+            $('input[type="text"]').addClass("is-invalid");
             $('#username-empty').removeAttr('hidden');
             $('#user-invalid').removeAttr('hidden');
         } else if (username !== 'new_user') {
-            $('input[type="text"]').css("border","1px solid red");
+            $('input[type="text"]').addClass("is-invalid");
             $('#username-valid').removeAttr('hidden');
             $('#user-invalid').removeAttr('hidden');
         } else {
-            $('input[type="text"]').css("border","1px solid green");
+            $('input[type="text"]').addClass("is-valid");
             $('#user-valid').removeAttr('hidden');
         }
 
         /* Password validation */
         if (password == null || password == '') {
-            $('input[type="password"]').css("border","1px solid red");
+            $('input[type="password"]').addClass("is-invalid");
             $('#password-empty').removeAttr('hidden');
             $('#pass-invalid').removeAttr('hidden');
         } else if (password !== '123456789') {
-            $('input[type="password"]').css("border","1px solid red");
+            $('input[type="password"]').addClass("is-invalid");
             $('#password-valid').removeAttr('hidden');
             $('#pass-invalid').removeAttr('hidden');
         } else {
-            $('input[type="password"]').css("border","1px solid green");
+            $('input[type="password"]').addClass("is-valid");
             $('#pass-valid').removeAttr('hidden');
         }
 
         /* Successful login */
         if (username == 'new_user' && password == '123456789') {
-            $('input[type="text"]').css("border","1px solid green");
-            $('input[type="password"]').css("border","1px solid green");
+            $('input[type="text"]').addClass("is-valid");
+            $('input[type="password"]').addClass("is-valid");
             $('#success-login').removeAttr('hidden');
             $('#user-valid').removeAttr('hidden');
             $('#pass-valid').removeAttr('hidden');
